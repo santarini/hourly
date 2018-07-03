faea382179dd36469ab5420ae9bf6eb1be2caa68
Makoa <32736507+santarini@users.noreply.github.com>
2018-07-02 16:53:24 -1000

    Create data.py

---
+++ data.py
@@ -0,0 +1,58 @@
+import csv
+import itertools
+import re
+import os
+import datetime as dt
+import pandas_datareader.data as web
+
+batch = []
+
+#create source folder if it doesnt exist yet
+if not os.path.exists('StockDatabase'):
+    os.makedirs('StockDatabase')
+
+with open("tester.csv", encoding='utf-8') as csvfile:
+    reader = csv.reader(csvfile)
+##    rowCount = sum(1 for row in reader)
+    for row in itertools.islice(reader, 0, 10):
+        for innerStr in row:
+            batch.append(innerStr)
+    print(batch)
+
+            #rawRow = re.search('[(.*)]', row)
+        #print(rawRow)
+
+
+##with open("tester.csv", encoding='utf-8') as csvfile:
+##    reader = csv.reader(csvfile)
+##    rowCount = sum(1 for row in reader)
+##    qtyHundreds = rowCount/100
+##
+####    if qtyHundreds > 1:
+####        #do some opeartion
+##
+##    for row in itertools.islice(reader, 0, 50):
+##        print(row)
+
+
+##
+##i = 2
+##
+###open csv
+##with open("tester.csv", encoding='utf-8') as csvfile:
+##    reader = csv.DictReader(csvfile)
+##    rowCount = sum(1 for row in reader)
+##    qtyHundreds = rowCount/100
+##    for i, row in enumerate(reader):
+##        if i == N:
+##            print("This is the line.")
+##            print(row)
+##            break
+##            
+##    print(rowCount)
+##    print(round(qtyHundreds))
+##
+###count number of cells in column
+###i = number of cells in column/100
+###j = 1
+###
