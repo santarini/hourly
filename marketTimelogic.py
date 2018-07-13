efb94962e93126415204eec87e207cbbd5a8f6a4
Makoa <32736507+santarini@users.noreply.github.com>
2018-07-12 17:15:08 -1000

    Update marketTimelogic.py

--- marketTimelogic.py
+++ marketTimelogic.py
@@ -1,15 +1,24 @@
-def job():
-    k = datetime.datetime.now()
-    print("Test " + str(k))
+import datetime
 
-for i in range(16,17):
-    for j in range(21,30,1):
-        if j == 0:
-            timeHolder = (str(i) + ":" + str(j) + "0")
-            schedule.every().day.at(timeHolder).do(job)
-        else:
-            timeHolder = (str(i) + ":" + str(j))
-            schedule.every().day.at(timeHolder).do(job)
+#NYSE,NASDAQ,AMEX (Hours 9:30am-4:00pm)
+
+weekdays = ['monday','tuesday','wednesday','thursday','friday']
+
+def job():
+    n = datetime.datetime.now()
+    print("Test " + str(n))
+
+for i in range(17,18):
+    for j in range(6,11,1):
+        for k in range(1,4):
+            if j == 0:
+                timeHolder = (str(i) + ":" + str(j) + "0")
+                dayHolder = weekdays[k]
+                schedule.every().dayHolder.at(timeHolder).do(job)
+            else:
+                timeHolder = (str(i) + ":0" + str(j))
+                dayHolder = weekdays[k]
+                schedule.every().dayHolder.at(timeHolder).do(job)
             
 while True:
     schedule.run_pending()
